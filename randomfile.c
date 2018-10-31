d077e0215e948395d32500ab286e3524dadc19c6
Aaron Li <aaronli39@gmail.com>
2018-10-30 20:26:26 -0400

    changes

--- randomfile.c
+++ randomfile.c
@@ -19,9 +19,22 @@ int main() {
         arr[i] = random_num();
         printf("arr[%d]: %d\n", i, arr[i]);
     }
-    i = & arr;
-    int out = open("temp.txt", O_CREAT | O_WRONLY, 0711);
-    write(out, i, 10 * sizeof(int));
 
+    // write to file
+    int dest = open("temp.txt", O_CREAT | O_WRONLY, 0777);
+    write(dest, arr, sizeof(arr));
+    printf("writing to file...\n\n");
+    close(dest);
+
+    // read from file
+    int * data = malloc(4);
+    dest = open("temp.txt", O_RDONLY);
+    read(dest, data, sizeof(arr));
+    printf("reading from file...\n\n");
+    close(dest);
+    printf("results: \n");
+    for(i = 0; i < 10; i++) {
+        printf("val: %d\n", * (data + i));
+    }
 
 }
